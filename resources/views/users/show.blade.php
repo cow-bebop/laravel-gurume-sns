a4563eb9415a2a11c0843cd143762d575a1e5c3c
けお <o.kengo1987@gmail.com>
2020-05-15 17:01:17 +0900

    followボタンコンポーネントの作成とユーザー画面への追加

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -11,12 +11,19 @@
           <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
             <i class="fas fa-user-circle fa-3x"></i>
           </a>
+          @if( Auth::id() !== $user->id )
+          <follow-button
+            class="ml-auto"
+          >
+          </follow-button>
+          @endif
         </div>
         <h2 class="h5 card-title m-0">
           <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
             {{ $user->name }}
           </a>
         </h2>
+
       </div>
       <div class="card-body">
         <div class="card-text">
