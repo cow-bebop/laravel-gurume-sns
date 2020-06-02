64a585cdc8731032f8dd3b0a567a9829f72053b6
けお <o.kengo1987@gmail.com>
2020-06-03 07:34:53 +0900

    ユーザー画像のcssを修正

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -19,7 +19,7 @@
         <input type="text" name="display_name" class="form-control" required value="{{ $user->display_name ? $user->display_name : $user->name }}">
       </div>
       <div class="md-form">
-        <textarea id="form7" name="profile" class="md-textarea form-control" rows="5" value="{{ $user->profile }}"></textarea>
+        <textarea id="form7" name="profile" class="md-textarea form-control" rows="5">{{ $user->profile }}</textarea>
         <label for="form7">自己紹介</label>
       </div>
       <input type="submit" name="confirm" id="button" value="登録" />
